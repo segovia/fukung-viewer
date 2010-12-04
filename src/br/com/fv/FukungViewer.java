6dd6cd0dc80867e2b77594a193eb1b3060f85572
Gustavo Segovia <gustavo.segovia@gmail.com>
2010-12-04 21:22:53 -0200

    Fukung Viewer Hello World!

---
+++ src/br/com/fv/FukungViewer.java
@@ -0,0 +1,14 @@
+package br.com.fv;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class FukungViewer extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
